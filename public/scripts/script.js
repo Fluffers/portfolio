585280142bd79b0142bb819988f3ed0a1283a6cf
Kuba Juszczak <kuborok123434@gmail.com>
2021-02-15 22:54:14 +0100

    Server side
    created app.js with handlebars, bodyparser, express, and nodemailer. It renders the site, and sends emails properly.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -19,23 +19,25 @@ for (var i = 0; themeDots.length > i; i++) {
 
 function setTheme(mode) {
     if (mode == "light") {
-        document.getElementById('theme-style').href = 'style.css'
+        document.getElementById('theme-style').href = 'public/css/style.css'
     } else if (mode == "blue") {
-        document.getElementById('theme-style').href = 'blue.css'
+        document.getElementById('theme-style').href = 'public/css/blue.css'
     } else if (mode == "green") {
-        document.getElementById('theme-style').href = 'green.css'
+        document.getElementById('theme-style').href = 'public/css/green.css'
     } else if (mode == "purple") {
-        document.getElementById('theme-style').href = 'purple.css'
+        document.getElementById('theme-style').href = 'public/css/purple.css'
     }else if (mode == "yellow") {
-        document.getElementById('theme-style').href = 'yellow.css'
+        document.getElementById('theme-style').href = 'public/css/yellow.css'
     }
 
     localStorage.setItem('theme', mode)
 }
 
+
 // notification handling  
 
 let notification = document.getElementById('notification-bar')
+console.log(notification)
 if(notification.children[0].innerHTML != ""){
     notification.style.height="initial"; 
     notification.style.visibility="visible"; 
